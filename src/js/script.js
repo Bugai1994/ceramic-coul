bb0c937e9928e7ca2c3f672a5abd93e20a50bc28
Vitaliy <vitalik.bugai@gmail.com>
2025-08-16 00:53:01 +0300

    v3

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,36 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
 import "purecss/build/grids-min.css";
 import "purecss/build/grids-responsive-min.css";
 import "/src/sass/style.scss";
+try {
+  const swiper = new Swiper('.works__slider', {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+   breakpoints: {
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 5,
+      },
+      1920: {
+        spaceBetween: 35,
+      },
+    },
+    pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      modules: [Navigation, Pagination],
+  })
+
+} catch (e) {}
