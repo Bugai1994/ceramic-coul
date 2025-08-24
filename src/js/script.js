dbd6b369f304a1066580e1be9e643122b5e4048e
Bugai1994 <vitalik.bugai@gmail.com>
2025-08-24 23:58:54 +0300

    1-pege_finish

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,7 @@ import 'swiper/css/pagination';
 import "purecss/build/grids-min.css";
 import "purecss/build/grids-responsive-min.css";
 import "/src/sass/style.scss";
+
 try {
   const swiper = new Swiper('.works__slider', {
     slidesPerView: 1,
